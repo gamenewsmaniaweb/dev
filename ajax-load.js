a58331e075bbbe02b5c67803d76fd8ae07c726d5
Game News Mania <94456855+gamenewsmaniaweb@users.noreply.github.com>
2023-02-24 11:58:00 +0530

    Create ajax-load.js

---
+++ ajax-load.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($) {
+    $('a').click(function(e) {
+        var link = $(this).attr('href');
+        if (location.hostname === this.hostname && link.indexOf("#") !== 0 && !$(this).hasClass('no-ajax')) {
+            e.preventDefault();
+
+            $.ajax({
+                url: link,
+                data: {
+                    page: 2
+                },
+                success: function(data) {
+                    // Replace the content of the current page with the loaded content
+                    $('body').html(data);
+
+                    // Change the URL displayed in the browser bar
+                    history.pushState(null, null, link);
+                },
+                error: function() {
+                    // Show an error message if the page fails to load
+                    alert('Sorry, the page could not be loaded.');
+                }
+            });
+        }
+    });
+});
